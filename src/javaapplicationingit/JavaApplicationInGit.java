c356a549db9190183bb2441d707ba74dc6de6770
SmartSan <smart100boyz@gmail.com>
2017-06-18 00:37:39 +0530

    Hello Sanjay

--- src/javaapplicationingit/JavaApplicationInGit.java
+++ src/javaapplicationingit/JavaApplicationInGit.java
@@ -15,6 +15,8 @@ public class JavaApplicationInGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("Hello");
+        
         // TODO code application logic here
     }
     
